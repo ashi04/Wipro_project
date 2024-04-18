c61780d0e231bb1c854bc35c806b5c56f9e622b5
Aswini <97080167+ashi04@users.noreply.github.com>
2024-04-18 08:49:27 +0530

    Add files via upload

---
+++ ASSIGNMENT/day05/task3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    printf("Enter the number of rows: ");
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+         char alphabet = 'A';
+
+         for (int j = 0; j < N - i - 1; j++)
+         {
+            printf(" ");
+         } 
+
+         for (int j = 0; j <= i; j++) 
+         {
+            printf("%c", alphabet++);
+         }
+
+        alphabet--; 
+        for (int j = 0; j < i; j++)
+        {
+            printf("%c", --alphabet);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
+      
